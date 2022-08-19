7855180bb111a3adf5844de9370182f628c09fe0
MelinaLisboa <melinalisboa@gmail.com>
2022-08-19 06:55:06 -0300

    Criação de Projeto com uso de React Router, que possibilita navegar entre os componentes.

--- navegacao/src/components/layout/Content.jsx
+++ navegacao/src/components/layout/Content.jsx
@@ -1,9 +1,23 @@
 import "./Content.css"
 import React from "react";
 
+import { Routes, Route } from "react-router-dom";
+
+import Home from "../../views/examples/Home";
+import Param from "../../views/examples/Param";
+import About from "../../views/examples/About";
+import NotFound from "../../views/examples/NotFound";
+
+
+
 const Content = props => (
     <main className="Content">
-        Componente Content
+        <Routes>
+            <Route path="/about" element={<About />} />            
+            <Route path="/param/:id" element={<Param />} />  {/* Uso de : indica que é uma variável */}
+            <Route exact path="/" element={<Home />} />
+            <Route path="*" element={<NotFound />} /> {/* O '*' indica que qq url não tratada acima, cai neste caso */}
+        </Routes>
     </main>
 )
 
--- navegacao/src/index.js
+++ navegacao/src/index.js
@@ -1,3 +1,4 @@
+import './index.css';
 import ReactDOM from 'react-dom';
 import React from 'react';
 
@@ -7,3 +8,6 @@ ReactDOM.render(
     <App />,
     document.getElementById('root')
 )
+
+// Dentro de view estão componentes que representam uma tela inteira
+// E dentro de components estão componentes em partes
