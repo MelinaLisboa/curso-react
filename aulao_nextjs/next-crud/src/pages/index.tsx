18b1b662ef199d10afed7e463c3e95d3b37953c6
MelinaLisboa <melinalisboa@gmail.com>
2022-09-20 14:23:48 -0300

    Criação e configuração componente Tabela.

--- aulao_nextjs/next-crud/src/pages/index.tsx
+++ aulao_nextjs/next-crud/src/pages/index.tsx
@@ -1,6 +1,24 @@
 import Layout from "../components/Layout"
+import Tabela from "../components/Tabela"
+import Cliente from "../core/Cliente"
 
 export default function Home() {
+
+  const clientes = [
+    new Cliente('Ana', 34, '1'),
+    new Cliente('Bia', 45, '2'),
+    new Cliente('Carlos', 23, '3'),
+    new Cliente('Pedro', 54, '4')
+  ]
+
+  function clienteSelecionado(cliente: Cliente) {
+      console.log(cliente.nome)
+  }
+
+  function clienteExcluido(cliente: Cliente) {
+      console.log(cliente.nome)
+  }
+
   return (
     <div className={`
       flex justify-center items-center 
@@ -8,7 +26,10 @@ export default function Home() {
       text-white
     `}>
       <Layout titulo="Cadastro Simples">
-        <span>Conteúdo</span>
+        <Tabela clientes={clientes} 
+          clienteSelecionado={clienteSelecionado}
+          clienteExcluido={clienteExcluido} 
+        />
       </Layout>
     </div>
   )
