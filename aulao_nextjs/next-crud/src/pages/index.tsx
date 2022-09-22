c81e8ebd301d713c298d2c953646671d64428cd6
MelinaLisboa <melinalisboa@gmail.com>
2022-09-22 16:25:35 -0300

    Integração do cadastro com o Firebase.

--- aulao_nextjs/next-crud/src/pages/index.tsx
+++ aulao_nextjs/next-crud/src/pages/index.tsx
@@ -3,20 +3,26 @@ import Tabela from "../components/Tabela"
 import Botao from "../components/Botao"
 import Formulario from "../components/Formulario"
 import Cliente from "../core/Cliente"
-import { useState } from "react"
+import { useEffect, useState } from "react"
+import ColecaoCliente from "../backend/db/ColecaoCliente"
+import ClienteRepositorio from "../core/ClienteRepositorio"
 
 export default function Home() {
 
+  const repo: ClienteRepositorio = new ColecaoCliente()
+
   const [cliente, setCliente] = useState<Cliente>(Cliente.vazio())
+  const [clientes, setClientes] = useState<Cliente[]>([])
   const [visivel, setVisivel] = useState<'tabela' | 'form'>('tabela')
 
+  useEffect(obterTodos, [])
 
-  const clientes = [
-    new Cliente('Ana', 34, '1'),
-    new Cliente('Bia', 45, '2'),
-    new Cliente('Carlos', 23, '3'),
-    new Cliente('Pedro', 54, '4')
-  ]
+  function obterTodos() {
+    repo.obterTodos().then(clientes => {
+      setClientes(clientes)
+      setVisivel('tabela')
+    })
+  }
 
   function novoCliente() {
     setCliente(Cliente.vazio())
@@ -28,13 +34,14 @@ export default function Home() {
       setVisivel('form')
   }
 
-  function clienteExcluido(cliente: Cliente) {
-      console.log(cliente.nome)
+  async function clienteExcluido(cliente: Cliente) {
+    await repo.excluir(cliente)
+    obterTodos()
   }
 
-  function salvarCliente(cliente: Cliente) {
-    console.log(cliente)
-    setVisivel('tabela')
+  async function salvarCliente(cliente: Cliente) {
+    await repo.salvar(cliente)
+    obterTodos()
   }
 
   return (
