3fd43efcc29b5bf33bad3b38bfad7f7e7b969118
MelinaLisboa <melinalisboa@gmail.com>
2022-10-04 18:49:23 -0300

    Criação componente pageHeader.

--- todo-app/frontend/src/about/about.jsx
+++ todo-app/frontend/src/about/about.jsx
@@ -1,7 +1,15 @@
 import React from "react"
+import PageHeader from "../template/pageHeader"
 
 export default props => (
     <div>
-        <h1>Sobre</h1>
+        <PageHeader name='Sobre' small='Nós'></PageHeader>
+        <h2>Nossa História</h2>
+        <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Quidem animi est cum quos tempora fugiat explicabo magni dignissimos nesciunt nihil modi tempore recusandae, nostrum ea a perspiciatis voluptate libero eaque?</p>
+        <h2>Misão e Visão</h2>
+        <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Architecto inventore ducimus, excepturi natus blanditiis alias laborum magni ratione perferendis reiciendis animi quibusdam dolore adipisci reprehenderit omnis debitis deleniti nostrum modi.
+        </p>
+        <h2>Imprensa</h2>
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Adipisci, ex eum earum fuga minus, facilis magnam nam dolorem temporibus animi consequuntur iusto incidunt laborum maxime suscipit iste nisi sapiente repudiandae.</p>
     </div>
 )
