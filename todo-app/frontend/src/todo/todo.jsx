7b167f6e694a0ba88c3e134ce6f2bdafe49f83ef
MelinaLisboa <melinalisboa@gmail.com>
2022-10-05 15:59:49 -0300

    Eventos Add e Change.

--- todo-app/frontend/src/todo/todo.jsx
+++ todo-app/frontend/src/todo/todo.jsx
@@ -4,11 +4,32 @@ import TodoForm from "./todoForm"
 import TodoList from "./todoList"
 
 export default class Todo extends Component {
+
+    constructor(props) {
+        super(props)
+        this.state = { description: '', list: [] }
+
+        //independente de quem chamar, é feito bind do this com o componente corrente
+        this.handleChange = this.handleChange.bind(this)
+        this.handleAdd = this.handleAdd.bind(this)
+        
+    }
+
+    handleChange(e) {
+        this.setState({...this.state, description: e.target.value})
+    }
+
+    handleAdd() {
+        console.log("Add")
+    }
+
     render() {
         return(
             <div>
                 <PageHeader name='Tarefas' small='Cadastro'></PageHeader>
-                <TodoForm />
+                <TodoForm description={this.state.description}
+                    handleChange={this.handleChange}
+                    handleAdd={this.handleAdd}/>
                 <TodoList />
             </div>
         )
