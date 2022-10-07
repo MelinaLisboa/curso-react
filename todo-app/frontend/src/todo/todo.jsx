d03544d6da6256b018a49b748b810eb6ab96cd59
MelinaLisboa <melinalisboa@gmail.com>
2022-10-07 15:35:57 -0300

    Melhorias CSS, Método limpar formulário, teclas de atalho.

--- todo-app/frontend/src/todo/todo.jsx
+++ todo-app/frontend/src/todo/todo.jsx
@@ -20,6 +20,7 @@ export default class Todo extends Component {
         this.handleRemove = this.handleRemove.bind(this)
         this.handleMarkAsDone = this.handleMarkAsDone.bind(this)
         this.handleMarkAsPending = this.handleMarkAsPending.bind(this)
+        this.handleClear = this.handleClear.bind(this)
         
         this.refresh()
         
@@ -61,6 +62,10 @@ export default class Todo extends Component {
         .then(resp => this.refresh(this.state.description))
     }
 
+    handleClear() {
+        this.refresh()
+    }
+
     render() {
         return(
             <div>
@@ -69,12 +74,13 @@ export default class Todo extends Component {
                     description={this.state.description}
                     handleChange={this.handleChange}
                     handleAdd={this.handleAdd} 
-                    handleSearch={this.handleSearch} />
+                    handleSearch={this.handleSearch}
+                    handleClear={this.handleClear} />
                 <TodoList 
                     list={this.state.list} 
                     handleRemove={this.handleRemove} 
                     handleMarkAsDone={this.handleMarkAsDone}
-                    handleMarkAsPending={this.handleMarkAsPending}/>
+                    handleMarkAsPending={this.handleMarkAsPending} />
             </div>
         )
     }
