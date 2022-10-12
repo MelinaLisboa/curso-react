5fd6c3c528b3a35e3391a237b9b6b383cf7a0ea6
MelinaLisboa <melinalisboa@gmail.com>
2022-10-12 10:01:31 -0300

    Criação dos componentes Media, Soma e Sorteio.

---
+++ redux/redux-simples/src/components/Sorteio.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+import Card from './Card'
+
+// eslint-disable-next-line import/no-anonymous-default-export
+export default props => {
+    return (
+        <Card title="Sorteio de um Número" purple>
+            <div>
+                <span>
+                    <span>Resultado:</span>
+                    <strong>8</strong>
+                </span>
+            </div>
+        </Card>
+    )
+}
