c773a272a9eef08ed99310f3657b056fcb94e467
MelinaLisboa <melinalisboa@gmail.com>
2022-10-18 07:23:41 -0300

    Organização e Melhorias.

--- redux/redux-simples/src/store/storeConfig.js
+++ redux/redux-simples/src/store/storeConfig.js
@@ -1,34 +1,9 @@
 import { combineReducers,  legacy_createStore as createStore } from 'redux'
+import numerosReducer from './reducers/numerosReducer'
 
 const reducers = combineReducers({
-    numeros: function(state, action) {
-        //console.log(state, ' ', action)
-        switch(action.type) {
-            case 'NUM_MIN_ALTERADO':
-               return {
-                ...state,
-                min: action.payload
-               } 
-            case 'NUM_MAX_ALTERADO':
-                return {
-                    ...state,
-                    max: action.payload
-                }
-            default:
-                return {
-                    min: 7,
-                    max: 31
-                }
-        }
-    },
-    nomes: function(state, action) {
-        //console.log(state, ' ', action)
-        return [
-            'Ana',
-            'Bia',
-            'Carlos'
-        ]
-    }
+    numeros: numerosReducer,
+    //demais reducers ficariam aqui!!!
 })
 
 function storeConfig() {
