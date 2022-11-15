4d3d40473332a94ac6498b350c0e21dbac887ec4
MelinaLisboa <melinalisboa@gmail.com>
2022-11-15 16:28:22 -0300

    Criação dos componentes de menu, sideBar e footer.

---
+++ my-money-app/frontend/src/common/template/footer.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+
+export default props => (
+    <footer className="main-footer">
+        <strong>
+            Copyright &copy; 2022
+            <a href="https://github.com/MelinaLisboa" target="_blank"> GitHub Melina Lisboa</a>.
+        </strong>
+    </footer>
+)
